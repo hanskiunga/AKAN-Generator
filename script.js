18d6c7fb76084ea2343f73748d1fa43f841091ef
hans <hakiunga@gmail.com>
2021-04-11 15:23:09 +0300

    Add formula to determine day of birth with sunday=1

--- script.js
+++ script.js
@@ -47,4 +47,7 @@ function dayValidator () {
 let monthValid=monthValidator();
 let dayValid=dayValidator();
 
+//formula to determine day of birth (Sunday=1, Monday=2)etc
+let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
+
 }
