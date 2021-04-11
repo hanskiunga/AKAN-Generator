fb1382e80b6e40cd0b6efd140817241fafc55ea2
hans <hakiunga@gmail.com>
2021-04-11 17:16:34 +0300

    Add arrays

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-function getAkanName () {
+function getAkanName () 
     let yearOfBirth = document.getElementById("year-input").value;
     let monthOfBirth = Number(document.getElementById("month-input") .value);
     let dayOfBirth = Number(document.getElementById("day-input").value);
@@ -42,7 +42,7 @@ function dayValidator () {
     }else {
         return true;
     }
-}
+
 //validation variables
 let monthValid=monthValidator();
 let dayValid=dayValidator();
@@ -50,4 +50,34 @@ let dayValid=dayValidator();
 //formula to determine day of birth (Sunday=1, Monday=2)etc
 let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
 
+//creating arrays of Akan names for males, females and days of week
+let daysOfWeek =[
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+
+let maleAkanNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+];
+let femaleAkanNames =[
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+];
+
+//generating an index value to select items on arrays
+let index;
+//fix formula bug
+if (dayOfWeekNumber ==0){
+    index =6;
+}else {
+    index = dayOfWeekNumber -1;
+}
+
+console.log(index);
+if (myGenderValue =="male" && monthValid && dayValid){
+   document.getElementById('result').textContent ="You were born on a " + daysOfWeek[index] + " , your Akan name is" + maleAkanNames[index];
+   document.getElementById('display-name').textContent = "Here is your Akan name: ";
+   document.getElementById('result').style.fontsize = "19px";
+   document.querySelector('h1').textContent = "Hello" + " " + femaleAkanNames[index];
+   return false;
+}else {
+    alert("You entered an invalid day or month, please try again");
 }
