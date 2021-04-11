dfe4411177bc5a06380694b5fc25a67cc9556ba9
hans <hakiunga@gmail.com>
2021-04-11 15:09:17 +0300

    Add day and month validators to script.js

---
+++ script.js
@@ -0,0 +1,45 @@
+
+function getAkanName () {
+    let yearOfBirth = document.getElementById("year-input").value;
+    let monthOfBirth = Number(document.getElementById("month-input") .value);
+    let dayOfBirth = Number(document.getElementById("day-input").value);
+    let genders = document.getElementsByName("gender");
+
+    //function to get gender
+    function getGender (){
+        for (let gender of genders){
+            if (gender.checked){
+                return gender.value;
+            }
+        }
+    }
+
+let myGenderValue = getGender();
+console.log(myGenderValue);
+
+//validation functions
+function monthValidator () {
+    if (monthOfBirth <1 || monthOfBirth >12){
+        return false;
+    } else{
+        return true;
+    }
+}
+
+function dayValidator () {
+    if (monthOfBirth === 2 && Number(yearOfBirth)%4 ===0{
+        if (dayOfBirth>28 || dayOfBirth <1){
+            return false;
+        }else if (monthOfBirth ===2 && dayOfBirth >29){
+            return false;
+        }else if (monthOfBirth ===2 && dayOfBirth <1){
+            return false;
+        }else {
+            return true;
+        }
+    }else if (dayOfBirth <1 || dayOfBirth >31){
+        
+    }
+}
+
+}
